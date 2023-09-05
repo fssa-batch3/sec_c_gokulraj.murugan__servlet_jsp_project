7d27bd744beec0f6dc10f2cc3286d7c57e90bba8
Gokulraj <110807453+Gokulraj-1@users.noreply.github.com>
2023-09-05 17:57:15 +0530

    Initial commit

---
+++ src/main/java/com/fssa/cryptocurrency/servlet/GetAllBookServlet.java
@@ -0,0 +1,34 @@
+package com.fssa.cryptocurrency.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class GetAllBookServlet
+ */
+@WebServlet("/GetAllBookServlet")
+public class GetAllBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String loggedInEmail = (String) session.getAttribute("loggedInEmail");
+		PrintWriter out = response.getWriter();
+		if (loggedInEmail == null) {
+			response.sendRedirect("login.html");
+		} else {
+			out.println("<h1>Printing all books</h1>");
+			out.println("<p>Logged In user: " + loggedInEmail + "</p>");
+			out.println("<a href='LogoutServlet'>logout</a>");
+		}
+	}
+
+}
